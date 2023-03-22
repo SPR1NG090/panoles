bcf26250634ad50408f8fbcb1cf1b100c3225696
Gijs van Vugt <gijs.van.vugt8@gmail.com>
2023-03-22 13:51:36 +0100

    infospots + title

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+
+//infospots
+var infospot, infospot2;
+
+infospot = new PANOLENS.Infospot();
+infospot.position.set( 5000.00, -665.23, -3996.49 );
+infospot.addHoverText( 'turkse pizza' );
+
+infospot2 = new PANOLENS.Infospot( 300, PANOLENS.DataImage.Info );
+infospot2.position.set( -5000.00, -1825.25, 197.56 );
+infospot2.addHoverElement( document.getElementById( 'infospot-1' ), 100 );
+
+
 // panolens select container
 const pan = document.querySelector('.pan');
 const img = './img/img1_panorama.jpg';
@@ -6,8 +19,12 @@ const viewer = new PANOLENS.Viewer({
     container: pan,
 })
 
-//run viewer
 
+
+//adding to objects
 viewer.add(panorama);
+panorama.add( infospot );
+panorama.add( infospot2 );
+
 
 console.log(viewer)
