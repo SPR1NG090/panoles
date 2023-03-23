a4fe72c90175b2b789e7c3b9f7d871faf38c50c9
Bjorn Meurkes <bjorn.meurkes@gmail.com>
2023-03-23 21:57:44 +0100

    nieuwe panorama's aan gemaakt met links ertussen

--- js/scene.js
+++ js/scene.js
@@ -4,33 +4,52 @@ var infospot, infospot2;
 const vrbutton = document.getElementsByClassName("vr-button");
 
 infospot = new PANOLENS.Infospot();
-infospot.position.set( 5000, 400, -400);
+infospot.position.set( 300, 450, -450);
 infospot.addHoverText( 'Tim zijn prachtige voorhoofd' );
 
 infospot2 = new PANOLENS.Infospot( 300, PANOLENS.DataImage.Info );
 infospot2.position.set( -5000.00, -1825.25, 197.56 );
 infospot2.addHoverElement( document.getElementById( 'infospot-1' ), 100 );
 
-
+var place = document.getElementById('place');
 //panolens select container
 
 //init externat constances
 const pan = document.querySelector('.pan');
 const img = './img/img8_panorama.jpg';
 const img2 = './img/img7_panorama.jpg';
+const img3 = './img/img6_panorama.jpg';
+const img4 = './img/img3_panorama.jpg';
+const img5 = './img/img5_panorama.jpg';
+const img6 = './img/img4_panorama.jpg';
 
 //init panorama + viewer
 const panorama = new PANOLENS.ImagePanorama(img);
 const panorama2 = new PANOLENS.ImagePanorama(img2);
+const panorama3 = new PANOLENS.ImagePanorama(img3);
+const panorama4 = new PANOLENS.ImagePanorama(img4);
+const panorama5 = new PANOLENS.ImagePanorama(img5);
+const panorama6 = new PANOLENS.ImagePanorama(img6);
 const viewer = new PANOLENS.Viewer({
     container: pan,
 })
 
 //linking between panorama's
-panorama.link( panorama2, new THREE.Vector3( -807.50, 404.88, 5000.00 ), 900, );
-panorama2.link( panorama, new THREE.Vector3( -807.50, 404.88, -5000.00 ), 300, );
-
+panorama.link( panorama2, new THREE.Vector3( 5000, 200, -400 ), 900, );
+panorama2.link( panorama, new THREE.Vector3( -5000, 100, 0 ), 900, );
+panorama2.link( panorama3, new THREE.Vector3( 5000, 200, -2000 ), 900,);
+panorama2.link( panorama4, new THREE.Vector3( 5000, 200, 3000 ), 900,);
+panorama3.link( panorama4, new THREE.Vector3( 0, 200, 5000 ), 900,);
+panorama3.link( panorama2, new THREE.Vector3( -5000, 100, -1000 ), 600,);
+panorama4.link( panorama2, new THREE.Vector3( -5000, 100, -1000 ), 600,);
+panorama4.link( panorama3, new THREE.Vector3( -2000, 100, -2000 ), 300,);
+panorama4.link( panorama5, new THREE.Vector3( 5000, 3000, 4000 ), 900,);
+panorama5.link( panorama6, new THREE.Vector3( -5000, -100, 0 ), 900,);
+panorama5.link( panorama4, new THREE.Vector3( -5000, -300, -4000 ), 900,);
+panorama6.link( panorama5, new THREE.Vector3( -5000, 100, -1000 ), 900,);
 var vrtoggled = false;
+
+panorama2.setLinkingImage('./img/img1.png');
 
 //toggle on/off vr mode
 function vrtoggle(){
@@ -44,9 +63,8 @@ function vrtoggle(){
     }
 }
 
-
 //adding to objects
-viewer.add(panorama, panorama2);
+viewer.add(panorama, panorama2, panorama3, panorama4, panorama5, panorama6);
 panorama.add( infospot );
 panorama.add( infospot2 );
 
