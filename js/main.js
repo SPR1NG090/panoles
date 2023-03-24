e4734abcfc6625d4dca2e80e01c67f83d8f0f5d1
Bjorn Meurkes <bjorn.meurkes@gmail.com>
2023-03-25 00:10:14 +0100

    stopwatch + plaats toegevoegd

--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
+const startButton = document.querySelector('#startButton');
+const startScherm = document.querySelector('#startScherm');
+
+const stopwatch = { elapsedTime: 0 };
+var milliseconds = 0;
+var seconds = 0;
+var minutes = 0;
+var hour = 0;
+let stop = false;
+
+startButton.addEventListener('click', () => {
+    startStopwatch();
+    startScherm.classList.add("hidden");
+  })
+
+function startStopwatch() {
+    if (stop == true) {
+      return;
+    }
+    else {
+      //reset start time
+      stopwatch.startTime = Date.now();
+      // run `setInterval()` and save the ID
+      stopwatch.intervalId = setInterval(() => {
+        //calculate elapsed time
+        const elapsedTime = Date.now() - stopwatch.startTime + stopwatch.elapsedTime
+        //calculate different time measurements based on elapsed time
+        milliseconds = parseInt((elapsedTime%1000)/10)
+        seconds = parseInt((elapsedTime/1000)%60)
+        minutes = parseInt((elapsedTime/(1000*60))%60)
+        hour = parseInt((elapsedTime/(1000*60*60))%24);
+        }, 100);
+      }
+  }
+
+function stopStopwatch() {
+    console.log(hour, minutes, seconds, milliseconds);
+    console.log("hello");
+}
+
+
+stopButton.addEventListener('click', () => {
+    stopStopwatch();
+})
 
--- js/scene.js
+++ js/scene.js
@@ -31,11 +31,11 @@ const panorama4 = new PANOLENS.ImagePanorama(img4);
 const panorama5 = new PANOLENS.ImagePanorama(img5);
 const panorama6 = new PANOLENS.ImagePanorama(img6);
 const viewer = new PANOLENS.Viewer({
-    container: pan,
+    container: pan, outpus: 'console'
 })
 
 //linking between panorama's
-panorama.link( panorama2, new THREE.Vector3( 5000, 200, -400 ), 900, );
+panorama.link( panorama2, new THREE.Vector3( 5000, 200, -400 ), 900,);
 panorama2.link( panorama, new THREE.Vector3( -5000, 100, 0 ), 900, );
 panorama2.link( panorama3, new THREE.Vector3( 5000, 200, -2000 ), 900,);
 panorama2.link( panorama4, new THREE.Vector3( 5000, 200, 3000 ), 900,);
@@ -49,7 +49,6 @@ panorama5.link( panorama4, new THREE.Vector3( -5000, -300, -4000 ), 900,);
 panorama6.link( panorama5, new THREE.Vector3( -5000, 100, -1000 ), 900,);
 var vrtoggled = false;
 
-panorama2.setLinkingImage('./img/img1.png');
 
 //toggle on/off vr mode
 function vrtoggle(){
@@ -68,6 +67,37 @@ viewer.add(panorama, panorama2, panorama3, panorama4, panorama5, panorama6);
 panorama.add( infospot );
 panorama.add( infospot2 );
 
+for (let index = 0; index < viewer.scene.children.length; index++) {
+    switch (index) {
+        case 0:
+            viewer.scene.children[index].name = "Buiten";
+            break;
+        case 1:
+            viewer.scene.children[index].name = "Hoofdingang";
+            break;
+        case 2:
+            viewer.scene.children[index].name = "Café";
+            break;
+        case 3:
+            viewer.scene.children[index].name = "Hal";
+            break;
+        case 4:
+            viewer.scene.children[index].name = "Studie ruimte";
+            break;
+        case 5:
+            viewer.scene.children[index].name = "Lokalen";
+            break;
+        default:
+            break;
+    }
+}
+this.setInterval(() => {
+    for (let index = 0; index < viewer.scene.children.length; index++) {
+        if (viewer.scene.children[index].active === true) {
+            place.innerHTML = viewer.scene.children[index].name;
+            // console.log (viewer.scene.children[index].name);
+        }
+    }
+}, 2000);
+ 
 
-
-console.log(viewer)
